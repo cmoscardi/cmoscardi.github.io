f3d4bf6de559fc1a5a03d53f50a75ab1a2737b74
Christian Moscardi <moscardi79@gmail.com>
2015-08-12 10:02:49 -0400

    adding blog page and logic

--- js/message.js
+++ js/message.js
@@ -1,3 +1,18 @@
+var QueryParams = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
+
 var fix_backspace = function() {
 
   $("body").keydown( function(event) {
@@ -47,7 +62,7 @@ var homepage_input_handler = function() {
           break;
         case "4":
           hide_all();
-          $("#blog").show();
+          window.location = "/blog/";
           break;
       }
       $("#input").text("");
