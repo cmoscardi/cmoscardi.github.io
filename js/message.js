b434218c88ac694b40073b529ba8a8f719bdb4f6
Christian Moscardi <moscardi79@gmail.com>
2015-08-12 21:30:01 -0400

    new JS to reflect the new nav UI logic

--- js/message.js
+++ js/message.js
@@ -13,15 +13,6 @@ var QueryParams = (function(a) {
 })(window.location.search.substr(1).split('&'));
 
 
-var fix_backspace = function() {
-
-  $("body").keydown( function(event) {
-    if(event.which==8){
-      event.preventDefault();
-      $("#input").text("");
-    }
-  });
-}
 
 var index_input_handler = function() {
   $("#cursor").addClass("flash");
@@ -41,36 +32,21 @@ var hide_all = function() {
   $(".hidden").hide();
 }
 
-var homepage_input_handler = function() {
+var nav_input_handler = function() {
   $(".option").show();
   $("#cursor").addClass("flash");
 
   $("body").keydown( function(event) {
     if(event.keyCode==13){
-      switch($("#input").text()){
-        case "1":
-          hide_all();
-          $("#about").show();
-          break;
-        case "2": 
-          hide_all();
-          $("#links").show();
-          break;
-        case "3":
-          hide_all();
-          $("#projects").show();
-          break;
-        case "4":
-          hide_all();
-          window.location = "/blog/";
-          break;
-      }
-      $("#input").text("");
+      var choice = $("#input").text();
+      window.location = $("#" + choice + " a").attr('href');
+    }
+    else {
+      $("#input").text(String.fromCharCode(event.keyCode));
     }
 
     $("#input").text(String.fromCharCode(event.keyCode));
   });
-  fix_backspace();
 }
 
 
